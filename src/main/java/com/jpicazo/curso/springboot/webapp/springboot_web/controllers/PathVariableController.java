e16af28c67c9df05be2a6129722cade4a8c365a5
jpicazo <juliopicazo06@gmail.com>
2025-01-13 15:15:09 -0600

    feat(post): create post controler

--- src/main/java/com/jpicazo/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
+++ src/main/java/com/jpicazo/curso/springboot/webapp/springboot_web/controllers/PathVariableController.java
@@ -1,24 +1,37 @@
 package com.jpicazo.curso.springboot.webapp.springboot_web.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.jpicazo.curso.springboot.webapp.springboot_web.models.User;
+import org.springframework.web.bind.annotation.*;
 
 import com.jpicazo.curso.springboot.webapp.springboot_web.models.dto.ParamDto;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/var")
 public class PathVariableController {
 
     @GetMapping("/baz/{message}")
-    public ParamDto bazDto(@PathVariable() String message) {
+    public ParamDto bazDto(@PathVariable(name = "message") String message) {
 
         ParamDto params = new ParamDto();
         params.setMassage(message);
         return params;
+    }
+
+    @GetMapping("/mix/{product}/{id}")
+    public Map<String, Object> mixPathVar(@PathVariable String product, @PathVariable Long id) {
+        Map<String, Object> json = new HashMap<>();
+        json.put("product", product);
+        json.put("id", id);
+
+        return json;
+    }
+
+    @PostMapping("/create")
+    public User create(@RequestBody User user){
+        return user;
     }
 
 }
--- src/main/java/com/jpicazo/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/jpicazo/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
+
 import java.util.List;
 import java.util.Map;
 import org.springframework.web.bind.annotation.RequestParam;
